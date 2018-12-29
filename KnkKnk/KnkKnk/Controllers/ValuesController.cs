d2b3d9e99e3277ecd3e619758f3207e5f17b9991
sunilmusinada <sunil.chaitanya@gmail.com>
2018-12-29 08:21:30 +0530

    initialpush

---
+++ KnkKnk/KnkKnk/Controllers/ValuesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KnkKnk.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnkKnk.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("Values")]
+        public string Values()
+        {
+            return "Knock Knock";
+        }
+        [Route("ReverseWords")]
+        public ActionResult<string> ReverseWords([FromQuery] string sentence)
+        {
+
+            string reverseString = string.Empty;
+            try
+
+            {
+                if (string.IsNullOrEmpty(sentence))
+                    return string.Empty;
+                reverseString = new string(sentence.Reverse().ToArray());
+            }
+            catch (Exception ex)
+            {
+                this.NotFound();
+            }
+            return this.Ok(reverseString);
+        }
+        [Route("Fibonacci")]
+        public ActionResult<long> Fibonacci([FromQuery] int n)
+        {
+            if (n > 92)
+                return this.NotFound();
+            Dictionary<int, long> fibDictionary = new Dictionary<int, long>();
+            try
+            {
+                fibDictionary.Add(0, 0);
+                fibDictionary.Add(1, 1);
+                FibCalc(n, ref fibDictionary);
+                long fib = fibDictionary.FirstOrDefault(e => e.Key.Equals(n)).Value;
+                return this.Ok(fib);
+            }
+            catch (Exception ex)
+            {
+                this.NotFound();
+            }
+            return this.Ok();
+        }
+        public static long FibCalc(int n, ref Dictionary<int, long> fibDictionary)
+        {
+            if (fibDictionary.Keys.Contains(n))
+                return fibDictionary[n];
+
+            long ans = FibCalc(n - 2, ref fibDictionary) + FibCalc(n - 1, ref fibDictionary);
+            fibDictionary.Add(n, ans);
+            return ans;
+
+
+        }
+        [Route("TriangleType")]
+        public ActionResult<string> TriangleType([FromQuery] int a, [FromQuery] int b, [FromQuery] int c)
+        {
+            Triangle triangle = new Triangle(a, b, c);
+            return triangle.TriangleType.ToString();
+
+        }
+
+        [Route("Token")]
+        public ActionResult<string> Token()
+        {
+            return "0073d45c-6b33-4b47-bd1e-6868a6086f15";
+        }
+    }
+}
---
+++ KnkKnk/KnkKnk/Model/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnkKnk.Model
+{
+    public enum TriangleType
+    {
+        Equilateral,
+        Isosceles,
+        Scalene,
+        Error
+
+    }
+    public class Triangle
+    {
+        private int _a { get; set; }
+        private int _b { get; set; }
+        private int _c { get; set; }
+
+        public TriangleType TriangleType { get; set; }
+        public Triangle(int a, int b, int c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+            FindTriangleType();
+        }
+        private void FindTriangleType()
+        {
+            if (_a <= 0 || _b <= 0 || _c <= 0)
+            {
+                TriangleType = TriangleType.Error;
+                return;
+            }
+            if (_a + _b <= _c || _b + _c <= _a || _c + _a <= _b)
+            {
+                TriangleType = TriangleType.Error;
+                return;
+            }
+            int[] sides = new int[] { _a, _b, _c };
+            int distinctSideCount = sides.Distinct().Count();
+            if (distinctSideCount == 1)
+            {
+                TriangleType = TriangleType.Equilateral;
+            }
+            if (distinctSideCount == 2)
+            {
+                TriangleType = TriangleType.Isosceles;
+                return;
+            }
+            if (distinctSideCount == 3)
+            {
+                TriangleType = TriangleType.Scalene;
+                return;
+            }
+        }
+    }
+}
